c0210ba64dfd16f5f2814ec98517c246eddc77f9
Davi Gasparino <dv.gasparino@gmail.com>
2023-05-29 19:38:07 -0300

    first commit

--- functions.php
+++ functions.php
@@ -204,10 +204,8 @@ add_filter( 'use_widgets_block_editor', '__return_false' );
 add_action( 'wp_enqueue_scripts', function() {
     // Remove CSS on the front end.
     wp_dequeue_style( 'wp-block-library' );
-
     // Remove Gutenberg theme.
     wp_dequeue_style( 'wp-block-library-theme' );
-
     // Remove inline global CSS on the front end.
     wp_dequeue_style( 'global-styles' );
 }, 20 );
