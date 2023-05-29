1f177e783f51892509012e7bb74dee141210e592
Davi Gasparino <dv.gasparino@gmail.com>
2023-05-29 19:41:21 -0300

    first commit

--- functions.php
+++ functions.php
@@ -196,18 +196,24 @@ function myFileUploaderRenderer()
 }
 
 add_shortcode('custom_file_uploader', 'myFileUploaderRenderer');
+
 // Disable Gutenberg on the back end.
 add_filter( 'use_block_editor_for_post', '__return_false' );
+
 // Disable Gutenberg for widgets.
 add_filter( 'use_widgets_block_editor', '__return_false' );
 
 add_action( 'wp_enqueue_scripts', function() {
+
     // Remove CSS on the front end.
     wp_dequeue_style( 'wp-block-library' );
+
     // Remove Gutenberg theme.
     wp_dequeue_style( 'wp-block-library-theme' );
+
     // Remove inline global CSS on the front end.
     wp_dequeue_style( 'global-styles' );
+
 }, 20 );
 
 add_action('dv_blank_brand_logo', 'add_brand_logo', 10, 1);
